a1db3200abbd5a9328ed38f2c09d7fc7b7196d5b
peng.xing <southeast_px@163.com>
2016-07-18 13:34:34 +0800

    conflict fixed

--- src/main/java/com/pauu/javase/TestJavaSe.java
+++ src/main/java/com/pauu/javase/TestJavaSe.java
@@ -5,6 +5,7 @@ public class TestJavaSe {
 	public static void main(String[] args) {
 		System.out.println("hello java!");
 		System.out.println("local update!!");
+		System.out.println("hello github!!");
 	}
 
 }
