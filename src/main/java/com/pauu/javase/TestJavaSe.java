153d8925190cc1c84bd0a736ddc139e874e90c11
southeastpx <987758324@qq.com>
2016-07-18 13:51:05 +0800

    Update TestJavaSe.java

--- src/main/java/com/pauu/javase/TestJavaSe.java
+++ src/main/java/com/pauu/javase/TestJavaSe.java
@@ -5,6 +5,7 @@ public class TestJavaSe {
 	public static void main(String[] args) {
 		System.out.println("hello java!");
 		System.out.println("hello github!!");
+		System.out.println("server edit!");
 	}
 
 }
